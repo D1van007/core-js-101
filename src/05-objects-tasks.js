ec5b62e1dc8776e068c3d44626badf08cc800cf0
D1van007 <d1van28.01.1987@gmail.com>
2022-12-01 21:16:24 +0300

    fix: rectangle function convert class

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -21,13 +21,15 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  // throw new Error('Not implemented');
-  this.width = width;
-  this.height = height;
-  Rectangle.prototype.getArea = function () {
-    return this.width * this.height;
-  };
+class Rectangle {
+  constructor(width, height) {
+    // throw new Error('Not implemented');
+    this.width = width;
+    this.height = height;
+    Rectangle.prototype.getArea = function () {
+      return this.width * this.height;
+    };
+  }
 }
 
 
